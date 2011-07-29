e545896395a38ab2f7d26a65830125a54e9fe9cc
Andrew Revell <arevell@gmail.com>
2011-07-29 17:03:59 +1200

    Removed file operations

--- _powerup/modules/PowerUpWeb/PowerUpWeb.psm1
+++ _powerup/modules/PowerUpWeb/PowerUpWeb.psm1
@@ -4,41 +4,6 @@ $sitesPath = "IIS:\sites"
 $appPoolsPath = "IIS:\apppools"
 $bindingsPath = "IIS:\sslbindings"
 
-function ReplaceDirectory([string]$sourceDirectory, [string]$destinationDirectory)
-{
-	if (Test-Path $destinationDirectory -PathType Container)
-	{
-		Write-Host "Removing folder"
-		Remove-Item $destinationDirectory -recurse -force
-	}
-	Write-Host "Copying files"
-	Copy-Item $sourceDirectory\ -destination $destinationDirectory\ -container:$false -recurse -force
-}
-
-function RobocopyDirectory([string]$sourceDirectory, [string]$destinationDirectory)
-{
-	Write-Host "copying newer files from $sourceDirectory to $destinationDirectory"
-	.\_powerup\robocopy.exe /E /np /njh /nfl /ns /nc $sourceDirectory $destinationDirectory 
-	
-	if ($lastexitcode -lt 8)
-	{
-		Write-Host "Successfully copied to $destinationDirectory "
-		cmd /c #reset the lasterrorcode strangely set by powershell to be non-0
-	}		
-}
-
-function Copy-MirroredDirectory([string]$sourceDirectory, [string]$destinationDirectory)
-{
-	Write-Host "Mirroring $sourceDirectory to $destinationDirectory"
-	.\_powerup\robocopy.exe /E /np /njh /nfl /ns /nc /mir $sourceDirectory $destinationDirectory 
-	
-	if ($lastexitcode -lt 8)
-	{
-		Write-Host "Successfully mirrored to $destinationDirectory "
-		cmd /c #reset the lasterrorcode strangely set by powershell to be non-0
-	}
-}
-
 function StopAppPoolAndSite($appPoolName, $siteName)
 {
 	StopAppPool($appPoolName)
