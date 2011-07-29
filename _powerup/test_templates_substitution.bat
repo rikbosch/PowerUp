6998474f1cd001ba8db5a45be81554a7c51974c8
Andrew Revell <arevell@gmail.com>
2011-07-29 18:24:16 +1200

    Cleaned up misc batch files

--- _powerup/test_templates_substitution.bat
+++ _powerup/test_templates_substitution.bat
@@ -1,11 +1,15 @@
+@echo off
 
 if not '%1'=='' goto RUN
 
 :NOENVIRONMENT
+	@echo on
 	echo Deployment environment parameter is required
-	echo e.g. test_templates production	
+	echo e.g. deploy production	
 	exit /B
 
 :RUN
-	powershell -inputformat none -command "Set-ExecutionPolicy Unrestricted"
+	call _powerup\ensure_prerequisites.bat
 	powershell -inputformat none -command ".\test_templates_substitution.ps1 -deploymentEnvironment %1 -destination ..\_templates_output -settingsFile ..\settings.txt -templatesFolder ..\_templates"
+	
+	
--- _powerup/test_templates_substitution.ps1
+++ _powerup/test_templates_substitution.ps1
@@ -1,22 +1,13 @@
-# Helper script for those who want to run
-# psake without importing the module.
 param([string]$deploymentEnvironment, [string]$settingsFile, [string]$destination, [string]$templatesFolder)
-# use the following for mandatory params: 
-# [string]$buildFile = $(throw "-buildFile is required.")
 try {
 	$ErrorActionPreference='Stop'
 	$currentPath = Get-Location
 	
-	try{
-		Copy-Item $currentPath\_PowershellExtensions\* -destination C:\Windows\System32\WindowsPowerShell\v1.0\Modules -force -recurse
-		}
-	catch{}
-	
-	import-module C:\Windows\System32\WindowsPowerShell\v1.0\Modules\AffinityId\Id.PowershellExtensions.dll
+	$env:PSModulePath = $env:PSModulePath + ";$currentPath\_powerup\modules\"
+	import-module AffinityId\Id.PowershellExtensions.dll
 
 	Write-Host "DeploymentEnvironment: $deploymentEnvironment"
 	Write-Host "Destination: $destination"
-
 	
 	$params = get-parsedsettings $currentPath\$settingsFile $deploymentEnvironment 
 	copy-substitutedsettingfiles -templatesDirectory $currentPath\$templatesFolder -targetDirectory $destination -deploymentEnvironment $deploymentEnvironment -settings $params		
