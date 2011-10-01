73e6c44bdf8b9573ef0cc4682cc80daf545548c8
Andrew Revell <arevell@gmail.com>
2011-10-02 08:33:17 +1300

    Simplified basic sample to no longer do ssl

--- deploy.ps1
+++ deploy.ps1
@@ -13,7 +13,4 @@ task web-deploy  {
 
 	set-webapppool ${website.name} "Integrated" "v4.0"
 	set-website ${website.name} ${website.name} ${deployment.root}\${website.name} "" "http" "*" ${http.port} 	
-	set-selfsignedsslcertificate ${website.name}
-	set-sslbinding ${website.name} "*" ${https.port} 
-	new-websitebinding ${website.name} ""  "https" "*" ${https.port} 
 }
